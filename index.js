b4846c5fc63be799e86d3e62d61aa79ee5c5c2f8
lucasquearis <lucasquearis@hotmail.com>
2022-02-05 16:43:33 -0300

    configura logica para novos usuarios e lista usuarios online

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const http = require('http').createServer(app);
 
@@ -8,20 +9,29 @@ const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
-  }
+  },
 });
+
+let userList = [];
 
 io.on('connection', (socket) => {
-  socket.on('ping', (_message) => {
-    console.log(socket.id)
-  })
-});
+  io.emit('userList', userList);
 
+  socket.on('newUser', (name) => {
+    userList.push({ id: socket.id, name });
+    io.emit('userList', userList);
+    socket.broadcast.emit('messageNewUser', (`${name} acabou de se conectar! :)`));
+  });
+
+  socket.on('disconnect', () => {
+    const newList = userList.filter(({ id }) => id === socket.id);
+    userList = newList;
+    io.emit('userList', userList);
+  });
+});
 
 app.get('/', (_req, res) => {
   res.status(200).send('deu tudo certo');
 });
 
-
-
-http.listen(PORT, () => console.log(`Online na porta ${PORT}`));
+http.listen(PORT, () => global.console.log(`Online na porta ${PORT}`));
