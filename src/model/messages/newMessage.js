b62fccf7192297cee069f5749f4ddaaa9d1a0499
lucasquearis <lucasquearis@hotmail.com>
2022-02-06 01:17:03 -0300

    instala biblioteca fns para horario e imlementa horas no DB

--- src/model/messages/newMessage.js
+++ src/model/messages/newMessage.js
@@ -1,6 +1,7 @@
+const { format } = require('date-fns');
 const connection = require('../connection');
 
 const newMessage = (username, message) => connection()
-  .then((db) => db.collection('messages').insertOne({ username, message }));
+  .then((db) => db.collection('messages').insertOne({ username, message, time: format(new Date(), "hh:mm aaaaa'm'") }));
 
 module.exports = { newMessage };
--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,4 +1,5 @@
 const userList = [];
+const userTypingUsers = [];
 const newMessageService = require('../services/messages/newMessage');
 
 const newUser = (socket, io) => {
@@ -16,17 +17,35 @@ const disconnect = (socket, io) => {
   });
 };
 
-const newMessage = (socket) => {
+const newMessage = (socket, io) => {
   socket.on('newMessage', async ({ userName, message }) => {
     await newMessageService.newMessage(userName, message);
+    io.emit('message', 'new Message!');
+  });
+};
+
+const userTyping = (socket, io) => {
+  socket.on('typing', ({ typing, userName }) => {
+    if (typing) {
+      if (userTypingUsers.some((user) => user.userName === userName)) {
+        return io.emit('typingList', userTypingUsers);
+      }
+      userTypingUsers.push(userName);
+      return io.emit('typingList', userTypingUsers);
+    }
+    const userIndex = userTypingUsers.findIndex((user) => user.userName === userName);
+    userTypingUsers.splice(userIndex, 1);
+    return io.emit('typingList', userTypingUsers);
   });
 };
 
 module.exports = (io) => {
   io.on('connection', async (socket) => {
+    console.log(`USUÁRIO ENTROU: ${socket.id}`);
     io.emit('userList', userList);
     newUser(socket, io);
     disconnect(socket, io);
-    newMessage(socket);
+    newMessage(socket, io);
+    userTyping(socket, io);
   });
 };
