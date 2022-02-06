dc1fc655cb4a9bcc93ee449e917c81bf61283161
lucasquearis <lucasquearis@hotmail.com>
2022-02-06 18:20:06 -0300

    troca porta mongo atlas

--- src/model/connection.js
+++ src/model/connection.js
@@ -6,14 +6,11 @@ const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
-
-const MONGO_DB_URL = `mongodb+srv://lucasquearis:${process.env.MONGO_PASSWORD}@my-personal-chat.l4idh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 let db = null;
 
 const connection = () => (db
   ? Promise.resolve(db)
-  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  : MongoClient.connect(process.env.MONGO_DB_URI, OPTIONS)
     .then((conn) => {
       db = conn.db('my_personal_chat');
       return db;
